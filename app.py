55da2b1a45e983f6c724935b61d25c2c55986681
Nam Tran <tcnam2000@gmail.com>
2022-03-23 23:03:08 +0700

    add UPDATE method

--- app.py
+++ app.py
@@ -45,6 +45,19 @@ def delete(id):
     except:
         return "There was a problem deleting that task"
 
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task=ToDo.query.get_or_404(id)
+    if request.method=='POST' and request.form['content']!="":
+        task.content=request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return redirect('/')
+    else:
+        return render_template('update.html',task=task)
+
     
 
 if __name__=="__main__":
